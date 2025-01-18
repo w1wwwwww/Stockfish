fdca1a6c4cf19ff53a4e0f19ea62aa481750ab04
Jake Senne <grover7677@icloud.com>
2025-01-18 01:02:26 -0600

    bench 1215847

--- src/search.cpp
+++ src/search.cpp
@@ -328,7 +328,7 @@ void Search::Worker::iterative_deepening() {
                 // Adjust the effective depth searched, but ensure at least one
                 // effective increment for every four searchAgain steps (see issue #2717).
                 Depth adjustedDepth =
-                  std::max(1, rootDepth - failedHighCnt - 3 * (searchAgainCounter + 1) / 4);
+                  std::max(0, rootDepth - failedHighCnt - 3 * (searchAgainCounter + 1) / 4);
                 rootDelta = beta - alpha;
                 bestValue = search<Root>(rootPos, ss, alpha, beta, adjustedDepth, false);
 
